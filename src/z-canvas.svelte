389335f44dc1a0b735414c1444120a7505c4cb45
Bernát Kalló <kallo.bernat@gmail.com>
2020-08-25 22:36:11 +0200

    two canvases (not now)

--- src/z-canvas.svelte
+++ src/z-canvas.svelte
@@ -1,25 +1,18 @@
 <script>
   import { ORIGIN } from './math/Point'
   import { onMount, tick } from 'svelte'
-  let hostGetter
-  let view
-  onMount(async () => {
-    await new Promise(resolve => setTimeout(resolve, 0))
-    view = hostGetter.parentNode.host.parentNode.closest('z-viewport').view
-  })
+  import { AnimatedPoint } from './animation/AnimatedPoint'
+  export let view = new AnimatedPoint(ORIGIN)
 </script>
 
 <svelte:options tag="z-canvas" />
-<x-host-getter bind:this={hostGetter} />
 
-{#if $view}
-  <div
-    style="transform-origin: 0 0; transform: translate({$view.x}px,{$view.y}px)scale({$view.scale});
-    --z-zoom: {$view.scale}; --z-zoom-0_2: {$view.scale ** 0.2}; --z-zoom-0_7: {$view.scale ** 0.7};
-    --z-delay: -{$view.scale / 100}s ">
-    <slot />
-  </div>
-{/if}
+<div
+  style="transform-origin: 0 0; transform: translate({$view.x}px,{$view.y}px)scale({$view.scale});
+  --z-zoom: {$view.scale}; --z-zoom-0_2: {$view.scale ** 0.2}; --z-zoom-0_7: {$view.scale ** 0.7};
+  --z-delay: -{$view.scale / 100}s ">
+  <slot />
+</div>
 
 <style>
   :host {
--- src/z-viewport.svelte
+++ src/z-viewport.svelte
@@ -1,36 +1,17 @@
 <script>
   import { Point, ORIGIN } from './math/Point'
-  import { AnimatedPoint } from './animation/AnimatedPoint'
   import { easeOutQuad } from 'penner'
-  export let view = new AnimatedPoint(ORIGIN)
-
-  function tap() {
-    view.animate(new Point(80, 40, 8.2), {
-      duration: 1500,
-      easing: easeOutQuad,
-      options: { path: 'euclidean' }
-    })
-  }
 </script>
 
 <svelte:options tag="z-viewport" />
-<x-backdrop on:click={tap} />
+
 <slot />
 
 <style>
-  x-backdrop {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
   :host {
     display: block;
     width: 400px;
     height: 300px;
-    align-items: stretch;
-    justify-items: stretch;
     overflow: hidden;
     contain: content;
   }
