85774cc1f727422ac65dc9d49c0de8f62d4e08e2
Bernát Kalló <kallo.bernat@gmail.com>
2020-10-12 22:34:20 +0200

    fix wheel

--- src/z-viewport.svelte
+++ src/z-viewport.svelte
@@ -1,18 +1,18 @@
 <script>
   import { get_current_component, onMount } from 'svelte/internal'
   import { writable } from 'svelte/store'
+  import { add, neg } from './math/points'
   const host = get_current_component()
   let view = (host.__z_view = writable({
     p: { x: 0, y: 0, k: 1 },
     options: { duration: 0 }
   }))
   host.addEventListener('wheel', event => {
+    const r = host.getBoundingClientRect()
+    const mouse = { x: event.clientX - r.left, y: event.clientY - r.top, k: 1 }
+    const wheel = { x: 0, y: 0, k: Math.pow(Math.SQRT2, -event.deltaY / 100) }
     $view = {
-      p: {
-        x: $view.p.x,
-        y: $view.p.y,
-        k: $view.p.k * Math.pow(Math.SQRT2, -event.deltaY / 100)
-      },
+      p: neg(add(add(add(neg($view.p), mouse), neg(wheel)), neg(mouse))),
       options: { duration: 250 }
     }
   })
